5f0584e17eb27e89674655b6ffda84f39e1e8f0e
chain8673 <90842262+chain8673@users.noreply.github.com>
2021-11-21 14:55:41 +0800

    Update UI_02.py
    增加了“描述”区域

--- UI_02.py
+++ UI_02.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
         data.append(textEdit.toPlainText())
         data.append(buttonGroup1.checkedButton().text())
         data.append(cb.currentText())
+        data.append(textEdit2.toPlainText())
         print(data)
         data.clear()
 
@@ -16,21 +17,17 @@ window.resize(1000, 600)
 window.move(600, 200)
 window.setWindowTitle('测试窗口')
 
+# 姓名
 label1 = QLabel(window)
 label1.setText('姓名：')
 label1.move(20, 30)
-
 textEdit = QTextEdit(window)
 textEdit.move(100, 30)
 
-button = QPushButton('Submit', window)
-button.move(300, 100)
-button.clicked.connect(test)
-
+# 性别选项，采用RadioButton方式
 label2 = QLabel(window)
 label2.setText('性别：')
 label2.move(20, 80)
-
 radioButton1 = QRadioButton('男', window)
 radioButton1.setChecked(True)
 radioButton2 = QRadioButton('女', window)
@@ -42,6 +39,7 @@ buttonGroup1.addButton(radioButton1)
 buttonGroup1.addButton(radioButton2)
 buttonGroup1.setExclusive(True)
 
+# 学历选项，采用下拉框方式，也就是ComboBox
 label3 = QLabel(window)
 label3.setText('学历：')
 label3.move(20, 160)
@@ -50,6 +48,19 @@ cb.move(100, 160)
 cb.addItem('高中')
 cb.addItem('本科')
 cb.addItem('研究生')
+
+# 描述
+label1 = QLabel(window)
+label1.setText('描述：')
+label1.move(20, 210)
+textEdit2 = QTextEdit(window)
+textEdit2.resize(300, 100)
+textEdit2.move(100, 210)
+
+# 按钮
+button = QPushButton('Submit', window)
+button.move(300, 100)
+button.clicked.connect(test)
 
 window.show()
 app.exec_()
